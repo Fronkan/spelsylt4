c9e2927012672f81de96359a57d556f7b4b8a6c0
Fredrik Bengtsson <fredrik.bengtsson94@gmail.com>
2020-05-27 22:18:33 +0200

    Add start an goal for player

--- src/scene1.ts
+++ src/scene1.ts
@@ -27,8 +27,8 @@ export class GameScene extends Phaser.Scene {
     storeTiles: Phaser.Tilemaps.Tileset;
     objectTiles: Phaser.Tilemaps.Tileset;
     walls: Phaser.Tilemaps.StaticTilemapLayer;
-    floor: Phaser.Tilemaps.StaticTilemapLayer;
     collectables: Phaser.Tilemaps.DynamicTilemapLayer;
+    goal: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
 
 preload() {
     this.load.tilemapTiledJSON("storeMap","assets/store_map.json");
@@ -49,23 +49,13 @@ constructor() {
 public create() {
     this.map = this.make.tilemap({key: "storeMap"});
     this.storeTiles =  this.map.addTilesetImage("store_background", "storeTiles");
-    this.floor = this.map.createStaticLayer("floor", this.storeTiles);
+    let floor = this.map.createStaticLayer("floor", this.storeTiles);
     this.walls = this.map.createStaticLayer("walls", this.storeTiles);
     this.walls.setCollisionBetween(1,1000, true, true);
     this.objectTiles = this.map.addTilesetImage("objects", "objects");
     this.collectables = this.map.createDynamicLayer("objects", this.objectTiles);
     this.collectables.setCollisionBetween(1, 1000, true, true);
-
-
-    // this.collectables = this.map.getObjectLayer("objects").objects;
-    // this.collectables.forEach(
-    //     obj =>{
-    //         obj.
-
-    //     }
-    // )
-    // this.floor.setScale(0.5,0.5);
-    // this.walls.setScale(0.5,0.5);
+    
 
     let rnd = new Phaser.Math.RandomDataGenerator();
     this.enemies = WorldSettings.enemyPaths.map(
@@ -74,17 +64,42 @@ public create() {
             let enemy = new Enemy(this, path.name, isInfected, path.duration, rnd.realInRange(0.1,0.7));
             return enemy
     });
-
-    // this.walls.forEachTile(
-    //     tile => {
-    //         if (tile.collides){
-    //             tile.setAlpha(0.1)
-    //         }
-    //     }
-    // )
     this.player = new Player(this);
-    this.scale.setGameSize(this.floor.displayWidth,this.floor.displayHeight);
-    this.physics.world.setBounds(0, 0, this.floor.displayWidth, this.floor.displayHeight);
+
+    console.log(this.map.objects);
+    const eventLayer: Phaser.Tilemaps.ObjectLayer = this.map.objects[0];
+    eventLayer.objects.forEach(
+        eventObj => {
+            console.log(eventObj);
+            if(eventObj.name == "start"){
+                let go = this.add.rectangle(
+                    eventObj.x+25,
+                    eventObj.y+10,
+                    eventObj.width,
+                    eventObj.height,
+                    0xFFFFFF
+                );
+                const center = go.getCenter();
+                this.player.go.setPosition(
+                    center.x,
+                    center.y
+                )
+            }
+            if(eventObj.name == "goal"){
+                let go = this.add.rectangle(
+                    eventObj.x+200,
+                    eventObj.y+50,
+                    eventObj.width,
+                    eventObj.height,
+                    0xFFFFFF
+                );
+                this.physics.add.existing(go);
+                this.goal = go as Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
+            }
+        }
+    )
+    this.scale.setGameSize(floor.displayWidth, floor.displayHeight);
+    this.physics.world.setBounds(0, 0, floor.displayWidth, floor.displayHeight);
     this.player.go.body.setCollideWorldBounds(true);
 }
 
@@ -94,22 +109,53 @@ isEnemyInfected(rnd: Phaser.Math.RandomDataGenerator, prob: integer){
     return role <= prob;
 }
 
-public update(dt) {
+public update(dt: number) {
     this.player.update(dt);
     this.physics.world.collide(this.player.go, this.walls);
     this.enemies = this.enemies.filter( enemy => !enemy.isDestroyed);
+    this.physics.overlap(this.player.go, this.goal,
+        () => {
+            this.add.text(
+                this.cameras.main.centerX - 200,
+                this.cameras.main.centerY- 200,
+                "VICTORY!",
+                {fontSize: '64px'}
+            );
+            const restartBtn = this.add.text(
+                this.cameras.main.centerX-100,
+                this.cameras.main.centerY-100,
+                'Restart Game',
+                { fill: '#0f0' }
+            );
+            restartBtn
+                .setInteractive()
+                .on("pointerover", () => restartBtn.setStyle({ fill: '#ff0'}))
+                .on('pointerout', () =>  restartBtn.setStyle({ fill: '#0f0'}))
+                .on('pointerdown', () =>  this.scene.restart());
+        }   
+    );
     this.enemies.forEach(
         enemy => {
             enemy.update();
             if (this.physics.world.overlap(this.player.go, enemy.go)){
                 if(this.player.infectionCheck(enemy)){
-                    this.add.text(300, 300, "Game Over", {fontSize: '64px'});
-                    const restartBtn = this.add.text(350, 400, 'Restart Game', { fill: '#0f0' });
+                    this.add.text(
+                        this.cameras.main.centerX - 200,
+                        this.cameras.main.centerY- 200,
+                        "Game Over",
+                        {fontSize: '64px'}
+                    );
+                    const restartBtn = this.add.text(
+                        this.cameras.main.centerX-100,
+                        this.cameras.main.centerY-100,
+                        'Restart Game',
+                        { fill: '#0f0' }
+                    );
                     restartBtn
                         .setInteractive()
                         .on("pointerover", () => restartBtn.setStyle({ fill: '#ff0'}))
                         .on('pointerout', () =>  restartBtn.setStyle({ fill: '#0f0'}))
-                        .on('pointerdown', () => this.scene.restart());
+                        .on('pointerdown', () =>  this.scene.restart());
                 }
             }
     });
